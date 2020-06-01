91eb11234ca34954a73644996b218b9b7eb818f6
Fatma Oymak <sugunum@gmail.com>
2020-06-01 22:01:45 +0300

    new final change

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -5,10 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+
 let myLaptop = {
     brand : "MacBook Air",
-    version : "MacOS Sierra 10.12.6"
-    screenSize : 13.3,
+    version : "MacOS Sierra 10.12.6",
+    screenSize : "13.3",
     memory : "8GB",
     processor : "1.7 GHz Intel i7"
 
