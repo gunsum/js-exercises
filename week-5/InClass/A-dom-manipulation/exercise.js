8c9f94dcb56f34d4fff1277cb33255def88a29e0
Fatma Oymak <sugunum@gmail.com>
2020-06-13 14:11:21 +0300

    last change saturday

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -4,6 +4,7 @@ Task 1
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
+    
 
     2. the first div element node
     --> should log the ".site-header" node
@@ -11,43 +12,69 @@ Write JavaScript below that logs:
     3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node
 
-    4. all the "p" elements of contained inside  the .primary-content element node
+    4. all the "p" elements of contained inside  
+    the .primary-content element node
     --> should log a list of nodes with a length of 3
 
 */
+            console.log(document.querySelectorAll('p'));
+
+
 
 /*
 Task 2
 ======
 
-When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
+When a user clicks the 'ALERT' button, 
+an alert box should 
+pop up with the text 
+"Thanks for visiting Bikes for Refugees!"
 */
+document.querySelector("#alertBtn")
+ .addEventListener('click',()=>{
+     alert("Thanks for visiting Bikes for Refugees!")});
+
 
 /*
 Task 3
 =======
 
-Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
+Write JavaScript below that
+changes the background colour of 
+the page when the 'Change colour'
+button is clicked.
 */
-
+document.querySelector("#bgrChangeBtn")
+ .addEventListener('click',(event)=>{
+     event.target.style.backgroundColor = "red";
+ })
 /*
 Task 4
 ======
 
-When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
+When a user clicks the 'Add some text' button,
+a new paragraph 
+should be added below the buttons 
+that says "Read more below."
 */
 
 /*
 Task 5
 ======
 
-When the 'Larger links!' button is clicked, the text of all links on the page should increase.
+When the 'Larger links!' button is clicked,
+the text of
+all links on the page should increase.
 */
 
 /*
 Task 6
 ======
 
-Using JavaScript, create an unordered list under the "Add" button.
-When the "Add" button is pressed, get the value of the text box on its left, and add it to the list you created above.
+Using JavaScript, create an unordered list
+under the "Add" button.
+When the "Add" button is pressed,
+get the value of the text box on 
+its left, and add it to the list you 
+created above.
 */
