52eec45087bb3d0e4ea923e5e7320b872c8de426
Fatma Oymak <sugunum@gmail.com>
2020-06-21 00:45:10 +0300

    new change

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -17,7 +17,15 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-            console.log(document.querySelectorAll('p'));
+    let task1 = document.querySelectorAll('p');
+    console.log(task1);
+    let task2 = document.querySelector(".site-header");
+    console.log(task2);
+    let task3 = document.querySelector("#jumbotron-text");
+    console.log(task3);
+    let task4 = document.querySelector(".primary-content p");
+    console.log(task4);
+
 
 
 
@@ -25,10 +33,7 @@ Write JavaScript below that logs:
 Task 2
 ======
 
-When a user clicks the 'ALERT' button, 
-an alert box should 
-pop up with the text 
-"Thanks for visiting Bikes for Refugees!"
+When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 document.querySelector("#alertBtn")
  .addEventListener('click',()=>{
@@ -44,10 +49,12 @@ changes the background colour of
 the page when the 'Change colour'
 button is clicked.
 */
+
 document.querySelector("#bgrChangeBtn")
  .addEventListener('click',(event)=>{
      event.target.style.backgroundColor = "red";
  })
+ 
 /*
 Task 4
 ======
